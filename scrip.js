4a86599729914cfb5c4783cbb24e91ff533840ba
Facundo Rañipe <ranipe_facundo@hotmail.com>
2024-09-29 22:58:57 -0300

    Avance JS

---
+++ scrip.js
@@ -0,0 +1,13 @@
+// Retive todo from local storage or initialize an empty array
+// Guarda toda la info de forma local para que cuando se vuelva a abrir la pagna todavia tenga almacenda las tareas que hice antes
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCist = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton")
+
+// Inicializar proyecto //queda al pendiente de evemntos que sucedan, queda "Escuchando" los posibles eventos
+document.addEventListener("DOMContentLoaded",function(){
+
+}) 
